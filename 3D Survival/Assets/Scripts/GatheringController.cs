bf104834a566b56bfe5cfb22aba4af89dd1df3d7
sevaluxx <48733664+sevaluxx@users.noreply.github.com>
2020-01-16 11:20:11 -0600

    pre-unity switch
    Added current progress before switching to unity collab.

---
+++ 3D Survival/Assets/Scripts/GatheringController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+This will change later on. 
+*/
+public class GatheringController : MonoBehaviour
+{
+    
+}
---
+++ 3D Survival/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    This is the Player Class. This script only holds 
+    data on the overall status of the player.
+*/
+public class Player : MonoBehaviour
+{
+    public float currentHealth;
+    public float maxHealth;
+    // public float speed;
+    public float currentHunger;
+    public float currentThirst;
+    public float bodyTemp;
+    public float energy;
+
+    private void Start() {
+        currentHealth = maxHealth;
+    }
+}
